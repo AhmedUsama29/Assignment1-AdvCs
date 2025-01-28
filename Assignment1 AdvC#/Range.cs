718b65dcd5cbf1ff82a5d15b527654143a959524
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-01-28 16:35:10 +0200

    Q2

---
+++ Assignment1 AdvC#/Range.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_AdvC_
+{
+    internal class Range<T> where T : IComparable<T>
+    {
+        public T Min { get; set; }
+        public T Max { get; set; }
+
+        public Range(T min, T max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+
+        public double Length()
+        {   
+            //idk how to deal with other types of T
+            double minimum = Convert.ToDouble(Min);
+            double maximum = Convert.ToDouble(Max);
+            return maximum - minimum;
+        }
+
+    }
+}
