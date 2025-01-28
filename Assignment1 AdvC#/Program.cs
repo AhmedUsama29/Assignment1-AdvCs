2e576af921d8d6a34c4ba3649cb65cc76ec6e00e
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-01-28 16:15:45 +0200

    Q1

--- Assignment1 AdvC#/Program.cs
+++ Assignment1 AdvC#/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr = { 15, 19, 8, 2, 5, 3, 11, 26, 21 };
+
+            BubbleSort(arr);
+
+            Console.WriteLine("Sorted Array:");
+
+            foreach (int item in arr)
+            {
+                Console.Write(item + ", ");
+            }
+
         }
+
+        #region Q1
+        public static void BubbleSort<T>(T[] array) where T : IComparable<T>
+        {
+            bool Swapped;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Swapped = false;
+
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        //Swap
+                        T temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        Swapped = true;
+                    }
+                }
+                if (!Swapped)
+                    break;
+            }
+        } 
+        #endregion
     }
 }
